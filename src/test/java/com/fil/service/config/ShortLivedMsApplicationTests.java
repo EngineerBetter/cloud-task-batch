18e027753c991d0bc90a36ac629b21d2e7d18d30
EngineerBetter <siemens-java@engineerbetter.com>
2017-09-28 10:36:01 +0100

    test that job launches successfully

--- src/test/java/com/fil/service/config/ShortLivedMsApplicationTests.java
+++ src/test/java/com/fil/service/config/ShortLivedMsApplicationTests.java
@@ -6,47 +6,33 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.EmbeddedDataSourceConfiguration;
-import org.springframework.cloud.task.configuration.EnableTask;
-import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-import com.fil.service.task.JobCompletionNotificationListener;
+import com.fil.service.config.ShortLivedMsApplicationTests.TestConfig;
 
 @RunWith(SpringRunner.class)
-@EnableAutoConfiguration
-@EnableBatchProcessing
-@EnableTask  
-@Import({EmbeddedDataSourceConfiguration.class,JobCompletionNotificationListener.class})
-@ContextConfiguration(classes=BatchJobConfiguration.class, loader=AnnotationConfigContextLoader.class)
+@ContextConfiguration(classes={TestConfig.class, BatchJobConfiguration.class})
 public class ShortLivedMsApplicationTests {
-
 	@Autowired
 	private JobLauncherTestUtils jobLauncherTestUtils;
-	
-	@Autowired
-	JdbcTemplate jdbcTemplate;
 
-	//@Test
+	@Test
 	public void testLaunchJob() throws Exception {
-
 		JobExecution jobExecution = jobLauncherTestUtils.launchJob();
 		assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
 
 	}
 
-	//@Test
-	public void testLaunchStep() {
-
-		JobExecution jobExecution = jobLauncherTestUtils.launchStep("readerStep");
-		assertEquals(BatchStatus.COMPLETED, jobExecution.getStatus());
+	@Configuration
+	public static class TestConfig {
+		@Bean
+		JobLauncherTestUtils testUtils() {
+			return new JobLauncherTestUtils();
+		}
 	}
-
 }
